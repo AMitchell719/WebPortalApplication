fa94790f95b53cb1bab18417d6f5abb3fb5eee1e
AMitchell719 <Aaron.Mitchell75@gmail.com>
2018-11-26 01:21:13 -0600

    Added more routes

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app.debug = True
 
 @app.route('/itadmin')
 def itadmin():
-        return render_template('it-admin.html')
+    return render_template('it-admin.html')
+
+@app.route('/ituser')
+def ituser():
+    return render_template('it-user.html')
+
+@app.route('/links_it')
+def links_it():
+    return render_template('links-it.html')
 
 @app.route('/redirect')
 def redirect():
@@ -23,6 +31,14 @@ def management():
 @app.route('/reset')
 def reset():
     return render_template('reset.html')
+
+@app.route('/updatelinks')
+def updatelinks():
+    return render_template('updatelinks.html')
+
+@app.route('/customer')
+def customer():
+    return render_template('customer.html')
 
 if __name__ == '__main__':
     app.run()
